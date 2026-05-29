0771077b6bc864d4cd5c2747dfdb1610c571c501
Banshidhar <mishramca2005@gmail.com>
2026-05-29 17:18:51 +0530

    added highllight directive

---
+++ src/app/show-error/show-error.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowError } from './show-error';
+
+describe('ShowError', () => {
+  let component: ShowError;
+  let fixture: ComponentFixture<ShowError>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ShowError],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ShowError);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/show-error/show-error.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-error',
+  imports: [],
+  templateUrl: './show-error.html',
+  styleUrl: './show-error.css',
+})
+export class ShowError {
+
+  
+}
